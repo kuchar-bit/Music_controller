ed8a016540a2f325b4e1a67f782d126775ade71d
Kuchar <sz.blazynski@gmail.com>
2021-03-10 23:51:42 +0100

    Add serializer to CreateRoom

--- api/views.py
+++ api/views.py
@@ -1,10 +1,8 @@
-from rest_framework import generics
-
+from rest_framework import generics, status
 from .models import Room
 from .serializers import RoomSerializer
-
-
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
