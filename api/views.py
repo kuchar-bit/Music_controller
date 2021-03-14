732bfa9c01537c524488f3b4b6e433130a6e4b70
Kuchar <sz.blazynski@gmail.com>
2021-03-14 19:52:22 +0100

    Add sessions and develop home page

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from .models import Room
 from .serializers import RoomSerializer, CreateRoomSerializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
+from django.http import JsonResponse
 
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
@@ -77,3 +77,14 @@ class CreateRoomView(APIView):
             return Response(RoomSerializer(room).data, status=status.HTTP_201_CREATED)
 
         return Response()
+
+class UserInRoom(APIView):
+
+    def get(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        data = {
+            'code': self.request.session.get('room_code')
+        }
+        return JsonResponse(data, status=status.HTTP_200_OK)
